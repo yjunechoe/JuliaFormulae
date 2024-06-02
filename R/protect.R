b410d7643207c36d679d09773dc910ab45375fce
June Choe <yjchoe@sas.upenn.edu>
2024-06-02 05:50:03 -0400

    only replace I with protect when its the caller

--- R/protect.R
+++ R/protect.R
@@ -1,7 +1,10 @@
 use_protect <- function(fm) {
   rrapply(
     fm,
-    condition = \(x) identical(x, quote(I)),
+    condition = \(x, .xsiblings) {
+      identical(x, quote(I)) &&
+        identical(.xsiblings[[1]], quote(I))
+    },
     f = \(x) quote(protect),
     how = "replace"
   )
